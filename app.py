f46671bcf656e50e4ffe71decf91bae41eff54cc
VortexInvestimentos <eduardodegobi@gmail.com>
2026-02-28 19:31:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 import math
-import base64
+import base64 
 
 # =========================================================
 # CONFIGURAÇÃO DA PÁGINA
